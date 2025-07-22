e9c57c9646be8a933984a02d489e02574f7ff460
ademuliadi <ademuliadi@email.com>
2025-07-22 09:43:08 +0800

    halaman login fix

--- healink-undiknas/src/app/app.routes.ts
+++ healink-undiknas/src/app/app.routes.ts
@@ -1,8 +1,15 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { Login } from './login/login';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
     { path: 'login', component: Login },
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: '**', redirectTo: 'login' }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
