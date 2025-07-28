0d8e9a06a0851b54197bdcd0639b6ee578844db4
ademuliadi <ademuliadi@email.com>
2025-07-28 15:05:49 +0800

    halaman doctordashboard belum fix

--- healink-undiknas/src/app/app.routes.ts
+++ healink-undiknas/src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { Doctordashboard } from './doctordashboard/doctordashboard';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'doctordashboard', pathMatch: 'full' },
+    { path: 'doctordashboard', component: Doctordashboard },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
