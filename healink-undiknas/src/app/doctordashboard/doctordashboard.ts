49e6f11243ab77be679ba47a66c1d79c6c0732be
ademuliadi <ademuliadi@email.com>
2025-07-29 16:07:31 +0800

    memperbaiki halaman doctor dashboard

--- healink-undiknas/src/app/doctordashboard/doctordashboard.ts
+++ healink-undiknas/src/app/doctordashboard/doctordashboard.ts
@@ -1,11 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-doctordashboard',
-  imports: [],
+  selector: 'app-doctor-dashboard',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './doctordashboard.html',
-  styleUrl: './doctordashboard.css'
+  styleUrls: ['./doctordashboard.css']
 })
 export class Doctordashboard {
-
+  patients: {
+  id: string;
+  age: number;
+  name: string;
+  diagnosis: string;
+  phone: string;
+}[] = [
+  {
+    id: '001',
+    age: 59,
+    name: 'John Doe',
+    diagnosis: 'Broken bone',
+    phone: '08311122333'
+  },
+  {
+    id: '002',
+    age: 59,
+    name: 'Lorem',
+    diagnosis: 'Ipsum',
+    phone: 'xxxxxxxxxxx'
+  }
+];
 }
