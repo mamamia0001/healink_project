27de36b9bfcf0bd3779868f945aeba83ed8523ed
ademuliadi <ademuliadi@email.com>
2025-08-07 11:08:19 +0800

    memperbaiki halaman doctordashboard

--- healink-undiknas/src/app/doctordashboard/doctordashboard.ts
+++ healink-undiknas/src/app/doctordashboard/doctordashboard.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-doctor-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './doctordashboard.html',
   styleUrls: ['./doctordashboard.css']
 })
@@ -59,4 +60,32 @@ export class Doctordashboard {
   closeModal() {
     this.selectedPatient = null;
   }
+
+  statistics2025 = [
+  { month: '01', count: 70 },
+  { month: '02', count: 55 },
+  { month: '03', count: 10 },
+  { month: '04', count: 60 },
+  { month: '05', count: 15 },
+  { month: '06', count: 9 },
+  { month: '07', count: 11 },
+  { month: '08', count: 13 },
+  { month: '09', count: 7 },
+  { month: '10', count: 14 },
+  { month: '11', count: 10 },
+  { month: '12', count: 12 }
+];
+
+getStatisticPolylinePoints(): string {
+  const maxY = 100;
+  const scaleY = 60 / maxY;
+  const stepX = 200 / (this.statistics2025.length - 1);
+  return this.statistics2025
+    .map((stat, i) => {
+      const x = i * stepX;
+      const y = 80 - (stat.count * scaleY);
+      return `${x},${y}`;
+    })
+    .join(' ');
+}
 }
